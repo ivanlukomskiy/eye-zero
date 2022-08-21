27af26fc67bfe49b9d2dc037220dd8bd24d3aa2b
ivanlukomskiy <ivanlukomskiy@yandex-team.ru>
2022-08-21 22:08:20 +0300

    debugging buffers

--- gst/gst.go
+++ gst/gst.go
@@ -10,6 +10,7 @@ package gst
 import "C"
 import (
 	"fmt"
+	"log"
 	"sync"
 	"time"
 	"unsafe"
@@ -45,10 +46,10 @@ func CreatePipeline(codecName string, tracks []*webrtc.TrackLocalStaticSample, p
 	pipelineStr := "appsink name=appsink"
 	var clockRate float32
 
-	//pipelineStr = "rpicamsrc bitrate=256000 preview=false ! video/x-h264, width=640, height=480, framerate=30/1, stream-format=byte-stream, profile=baseline ! h264parse ! video/x-h264, alignment=au, stream-format=byte-stream ! " + pipelineStr
+	pipelineStr = "rpicamsrc keyframe-interval=3 use-stc=false bitrate=256000 preview=false ! video/x-h264, framerate=1/1, stream-format=byte-stream, profile=baseline ! h264parse ! video/x-h264, alignment=au, stream-format=byte-stream ! " + pipelineStr
 
 	// works ok, but eats through cpu
-	pipelineStr = "rpicamsrc bitrate=256000 preview=false ! video/x-raw, width=640, height=480, framerate=30/1 ! x264enc speed-preset=ultrafast tune=zerolatency key-int-max=20  ! video/x-h264,stream-format=byte-stream ! " + pipelineStr
+	//pipelineStr = "rpicamsrc bitrate=256000 preview=false ! video/x-raw, width=640, height=480, framerate=30/1 ! x264enc speed-preset=ultrafast tune=zerolatency key-int-max=20  ! video/x-h264,stream-format=byte-stream ! " + pipelineStr
 
 	// works ok, but its a test source
 	//pipelineStr = "videotestsrc ! video/x-raw,format=I420 ! x264enc speed-preset=ultrafast tune=zerolatency key-int-max=20 ! video/x-h264,stream-format=byte-stream ! " + pipelineStr
@@ -96,7 +97,7 @@ func goHandlePipelineBuffer(buffer unsafe.Pointer, bufferLen C.int, duration C.i
 		for _, t := range pipeline.tracks {
 			sampleData := C.GoBytes(buffer, bufferLen)
 			sampleDuration := time.Duration(duration)
-			//log.Printf("Handling buffers, buffer len %d, duration %d mks", int(bufferLen), sampleDuration.Microseconds())
+			log.Printf("Handling buffers, buffer len %d, duration %d mks", int(bufferLen), sampleDuration.Microseconds())
 			if err := t.WriteSample(media.Sample{Data: sampleData, Duration: sampleDuration}); err != nil {
 				panic(err)
 			}

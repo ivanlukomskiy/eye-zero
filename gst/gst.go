5c93b5d5d742e29d50ea22cf5ea6d8a08a997f74
ivanlukomskiy <ivanlukomskiy@yandex-team.ru>
2022-08-21 21:50:29 +0300

    debugging buffers

--- gst/gst.go
+++ gst/gst.go
@@ -45,8 +45,7 @@ func CreatePipeline(codecName string, tracks []*webrtc.TrackLocalStaticSample, p
 	pipelineStr := "appsink name=appsink"
 	var clockRate float32
 
-	//pipelineStr = "rpicamsrc bitrate=256000 preview=false ! video/x-h264, width=640, height=480, framerate=30/1, stream-format=byte-stream, profile=baseline ! h264parse alignment=au, stream-format=byte-stream ! " + pipelineStr
-	pipelineStr = "rpicamsrc bitrate=256000 ! video/x-h264 ! h264parse alignment=nal, stream-format=byte-stream ! " + pipelineStr
+	pipelineStr = "rpicamsrc bitrate=256000 preview=false ! video/x-h264, width=640, height=480, framerate=30/1, stream-format=byte-stream, profile=baseline ! h264parse ! video/x-h264, alignment=au, stream-format=byte-stream ! " + pipelineStr
 
 	// works ok, but eats through cpu
 	//pipelineStr = "rpicamsrc bitrate=256000 preview=false ! video/x-raw, width=640, height=480, framerate=30/1 ! x264enc speed-preset=ultrafast tune=zerolatency key-int-max=20  ! video/x-h264,stream-format=byte-stream ! " + pipelineStr
